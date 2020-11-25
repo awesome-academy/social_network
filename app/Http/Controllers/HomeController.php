045735d4e05a3c577315e832b8331ce5366ba1b5
kienpham2004 <phamkien20041998@gmail.com>
2020-11-25 20:58:33 +0700

    require ui login of laravel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,23 @@ class HomeController extends Controller
     {
         return view('welcome');
     }
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,7 @@ Route::group(['middleware' => 'locale'], function () {
     Route::get('/home/{lang}', 'SwitchLanguage@setLocale')->name('home.lang');
     Route::get('/profile', 'HomeController@profile')->name('home.profile');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
